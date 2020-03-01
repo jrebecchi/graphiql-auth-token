5c28e5e37cfdcd6cf2d53fcb3efc830b9ed0d9b3
jrebecchi <johann.corcuff@gmail.com>
2020-03-01 16:06:29 +0100

    Cleaning demo code

--- demo/src/index.js
+++ demo/src/index.js
@@ -21,7 +21,6 @@ class Demo extends Component {
         this.socket = socketIOClient("http://localhost:43500");
         this.socket.on("notification", data => {
             if (Array.isArray(data)) {
-                console.log(data);
                 this.setState({ notifications: data })
             }
         });
