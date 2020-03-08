19f40fe8c9f5ff4314129da44a6a47c4a752a79c
jrebecchi <johann.corcuff@gmail.com>
2020-03-08 22:41:34 +0100

    Updating example with notification support

--- examples/ExampleWithExpressGraphQL.js
+++ examples/ExampleWithExpressGraphQL.js
@@ -1,10 +1,12 @@
 const accepts = require('accepts');
-const someGraphQLSchema = require('./someGraphQLSchema');
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('../demo/src/schema');
+const socketIo = require("socket.io");
 
 const app = express();
 
+//Overiding the default express-graphql middleware
 app.use('/graphql',  async (req, res, next) => {
     const params = await graphqlHTTP.getGraphQLParams(req);
     if (!params.raw && accepts(req).types(['json', 'html']) === 'html'){
@@ -14,15 +16,47 @@ app.use('/graphql',  async (req, res, next) => {
     else next();
 });
 
+//Sending the pure GraphQL requests to the express-graphql middleware
 app.use('/graphql', graphqlHTTP(async (req, res) => {
     return {
-        schema: someGraphQLSchema,
+        schema,
         graphiql: false,
     }})
 );
 
-app.listen(4000);
+const server = app.listen(4000);
 
+//Setting up the socket.io that will send notifications to the client
+const io = socketIo(server);
+let interval;
+let i = 0
+const types = ["", "secondary", "success", "info", "warning", "danger"];
+const randomType = () => types[(Math.floor(Math.random() * types.length))];
+const getApiAndEmit = async socket => {
+    try {
+        socket.emit("notification", {
+            message: 'A little message for you: <a href="/" target="_blank"/>click here</a>',
+            title: 'Message number ' + (++i),
+            type: randomType()
+        });
+    } catch (error) {
+        console.error(`Error: ${error.code}`);
+    }
+}
+
+io.on("connection", socket => {
+    console.log("New client connected");
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 5000);
+    socket.on("disconnect", () => {
+        console.log("Client disconnected");
+    });
+});
+
+
+// function rendering the webpage with graphiql-auth-token
 function renderGraphiQLAuthToken(data) {
     const queryString = data.query;
     const variablesString = data.variables != null ? JSON.stringify(data.variables, null, 2) : null;
@@ -52,13 +86,14 @@ function renderGraphiQLAuthToken(data) {
         width: 100vh;
       }
     </style>
-    <link rel="stylesheet" href="https://unpkg.com/graphiql-auth-token@1.0.1/umd/graphiql-auth-token.css"> 
+    <link rel="stylesheet" href="https://unpkg.com/graphiql-auth-token@1.1.2/umd/graphiql-auth-token.css"> 
     <script src="https://unpkg.com/promise-polyfill@8.1.3/dist/polyfill.min.js"></script>
     <script src="https://unpkg.com/unfetch@4.1.0/dist/unfetch.umd.js"></script>
-    <script src="https://unpkg.com/react@16.12.0/umd/react.production.min.js"></script>
-    <script src="https://unpkg.com/react-dom@16.12.0/umd/react-dom.production.min.js"></script>
-    <script src="https://unpkg.com/graphiql-auth-token@1.0.1/umd/graphiql-auth-token.min.js"></script>
-  </head>
+    <script src="https://unpkg.com/react@16.12.0/umd/react.development.js"></script>
+    <script src="https://unpkg.com/react-dom@16.12.0/umd/react-dom.development.js"></script>
+    <script src="https://unpkg.com/graphiql-auth-token@1.1.2/umd/graphiql-auth-token.min.js"></script>
+    <script src="https://unpkg.com/socket.io-client@2.3.0/dist/socket.io.slim.js"></script>
+    </head>
   <body>
     <div id="graphiql">Loading...</div>
     <script>
@@ -130,20 +165,45 @@ function renderGraphiQLAuthToken(data) {
       function updateURL() {
         history.replaceState(null, null, locationQuery(parameters));
       }
-      // Render <GraphiQL /> into the body.
-      ReactDOM.render(
-        React.createElement(GraphiQLAuthToken, {
-          fetcher: graphQLFetcher,
-          onTokenUpdate: onTokenUpdate,
-          onEditQuery: onEditQuery,
-          onEditVariables: onEditVariables,
-          onEditOperationName: onEditOperationName,
-          query: ${safeSerialize(queryString)},
-          variables: ${safeSerialize(variablesString)},
-          operationName: ${safeSerialize(operationName)},
-        }),
-        document.getElementById('graphiql')
-      );
+
+      class GraphiQLIDE extends React.Component {
+        
+        constructor() {
+            super();
+            this.state = {
+                notification: null,
+                query: ${safeSerialize(queryString)},
+            }
+        }
+        
+        componentDidMount() {
+          this.socket = io(fetchURL);
+          this.socket.on("notification", data => {
+              this.setState({ notification: data, query: parameters.query });
+          });
+        }
+
+        componentWillUnmount() {
+            this.socket.close();
+        }
+
+        render() {
+          return React.createElement(GraphiQLAuthToken, {
+            fetcher: graphQLFetcher,
+            onTokenUpdate: onTokenUpdate,
+            onEditQuery: onEditQuery,
+            onEditVariables: onEditVariables,
+            onEditOperationName: onEditOperationName,
+            query: this.state.query,
+            variables: ${safeSerialize(variablesString)},
+            operationName: ${safeSerialize(operationName)},
+            notification: this.state.notification
+          });
+        }
+      }
+
+      // Render <GraphiQLIDE /> into the body.
+      ReactDOM.render(React.createElement(GraphiQLIDE), document.getElementById('graphiql'));
     </script>
   </body>
   </html>`;
